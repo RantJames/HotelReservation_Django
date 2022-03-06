bb2c8ba0577a7fb56fa0ca1b996133241d0df1dd
RantJames <antony.rubin.95@gmail.com>
2022-03-06 15:08:44 -0400

    Commiting models, migrations and database settings

--- HotelReservation_Django/settings.py
+++ HotelReservation_Django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ReservationApp',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,17 @@ WSGI_APPLICATION = 'HotelReservation_Django.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ra_james',
+        'HOST': '127.0.0.1',
+        'PORT': '8001',
+        'USER': 'ra_james',
+        'PASSWORD': 'A00455851!cda'
     }
 }
 
--- ReservationApp/models.py
+++ ReservationApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class HotelList(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+    price = models.IntegerField()
+    last_updated = models.DateTimeField(default=timezone.now)
+
+
+class ReservationDetails(models.Model):
+    res_id = models.ForeignKey(HotelList, on_delete=models.CASCADE, primary_key=True)
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+
